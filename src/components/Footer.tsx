e503fe0d1d0452e06e98cc482f16e8860f99ecb0
maxga <maxga.p@topvalue.com>
2025-01-03 22:19:36 +0700

    link footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,21 +1,28 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { faCircle } from "@fortawesome/free-solid-svg-icons";
-import {
-  faInstagram,
-  faLinkedin,
-  faGithub,
-} from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <div className="container mx-auto">
       <div className="flex justify-between py-10">
         <h5>StyleDee</h5>
-        <div className="flex items-center space-x-5 *:size-6">
-          <FontAwesomeIcon icon={faInstagram} />
-          <FontAwesomeIcon icon={faLinkedin} />
-          <FontAwesomeIcon icon={faGithub} />
+        <div className="flex items-center space-x-5 [&_svg]:size-6">
+          <Link
+            href={"https://www.linkedin.com/in/puttapol-putthati-611048289"}
+            target="_blank"
+          >
+            <FontAwesomeIcon icon={faLinkedin} />
+          </Link>
+          <Link href={"https://github.com/putatpol"} target="_blank">
+            <FontAwesomeIcon icon={faGithub} />
+          </Link>
+          <Link href={"mailto:puttapol.put@gmail.com"} target="_blank">
+            <FontAwesomeIcon icon={faEnvelope} />
+          </Link>
         </div>
         <div className="flex items-center space-x-5 text-gray-400">
           <p>Front-end Project</p>
